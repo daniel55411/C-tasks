be38821ff9df16faac9c4fa97084308967ee4126
d4n13lzh3 <zhe.dan28@gmail.com>
2018-03-13 13:49:30 +0500

    Changed first task

--- right_triangle/main.c
+++ right_triangle/main.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 
+int cmp(float value_1, float value_2) {
+    int int_value_1 = *(int*)&value_1;
+    int int_value_2 = *(int*)&value_2;
+
+    if (int_value_1 == int_value_2) {
+        return 0;
+    } else if (int_value_1 > int_value_2) {
+        return 1;
+    } else if (int_value_1 < int_value_2) {
+        return -1;
+    }
+}
+
 int pythagorean_theorem(float catheter_1,
                         float catheter_2,
                         float hypotenuse){
-    return catheter_1 * catheter_1 + catheter_2 * catheter_2 == hypotenuse * hypotenuse;
+    int result = cmp(
+            catheter_1 * catheter_1 + catheter_2 * catheter_2,
+            hypotenuse * hypotenuse
+    );
+    return result == 0;
+}
+
+int is_triangle(float size_1, float size_2, float size_3) {
+    return cmp(size_1, size_2 + size_3) <= 0;
 }
 
 int main(void) {
     float side_1, side_2, side_3;
+
     scanf("%f %f %f", &side_1, &side_2, &side_3);
+
+    if (!is_triangle(side_1, side_2, side_3)) {
+        return 1;
+    }
+
     if (pythagorean_theorem(side_1, side_2, side_3)
         || pythagorean_theorem(side_1, side_3, side_2)
             || pythagorean_theorem(side_3, side_2, side_1)){
@@ -16,5 +43,6 @@ int main(void) {
     } else {
         puts("False");
     }
+
     return 0;
 }
